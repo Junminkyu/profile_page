87da2240046fb9744b7afdb96e3aa1b2e2f500b2
Junminkyu <mkjun0911@gmail.com>
2024-04-10 23:13:28 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -24,3 +24,25 @@ let swiperCards = new Swiper(".card__content", {
     },
   },
 });
+
+// document.querySelector(".flip-card").addEventListener("click",function(){
+//   const card=document.querySelector(".flip-card");
+//   if(card.style.transform!='rotateY(180deg)'){
+//     document.querySelector(".flip-card-inner").style.transform='rotateY(180deg)';
+//   }
+//   else{
+//     console.log('yo');
+//     document.querySelector(".flip-card-inner").style.transform='rotateY(0deg)';
+//   }
+// })
+
+function flip(clickedCard) {
+  const card = clickedCard.querySelector(".flip-card-inner");
+  const currentRotation = getComputedStyle(card).getPropertyValue("transform");
+
+  if (currentRotation === "none" || currentRotation === "matrix(1, 0, 0, 1, 0, 0)") {
+      card.style.transform = 'rotateY(180deg)';
+  } else {
+      card.style.transform = 'rotateY(0deg)';
+  }
+};
